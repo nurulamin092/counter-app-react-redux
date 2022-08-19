8a22807a5fd2c79fcbbbcbef75d18d4d9f31a08a
nurulamin092 <mnurulamin092@gmial.com>
2022-08-19 07:19:45 +0600

    use dispatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+import HooksCounter from './components/HooksCounter';
 import store from './redux/store';
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
           Simple Counter Application
         </h1>
         <div className="max-w-md mx-auto mt-10 space-y-5">
-          <Counter />
+          <HooksCounter />
         </div>
       </div>
     </Provider>
